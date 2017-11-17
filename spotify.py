b993dd4403e2358ed6b34e1b831007c09c74ceff
cselig <cselig@uw.edu>
2017-11-16 21:37:05 -0800

    vote to skip song 🤑🤑🤑

--- spotify.py
+++ spotify.py
@@ -5,21 +5,27 @@ import re
 import time
 import json
 
+from pprint import pprint
+
 class SpotifySlackBot():
-    def __init__(self, api_key, broadcast_channel):
+    def __init__(self, api_key, broadcast_channel, skips_needed=2):
         self.broadcast_channel = broadcast_channel
         self.sc = SlackClient(api_key)
 
         # Get the user list
         response = self.sc.api_call('users.list')
         self.users = json.loads(response)['members']
-        
+
+        # set of user ids who have requested to skip current song
+        # should reset every time a new song plays
+        self.skips = set()  
+        self.skips_needed = skips_needed
 
     def command_current_song(self, event):
         data = self.run_spotify_script('current-song')
         data = data.strip().split('\n')
         data = {"id": data[0], "name": data[1], "artist": data[2]}
-        message = "Hey, the current song is *%s* by *%s*. You can open it on Spotify: %s" % (data['name'], data['artist'], data['id'])
+        message = "The current song is *%s* by *%s*." % (data['name'], data['artist'])
         
         self.sc.rtm_send_message(event['channel'], message)
         
@@ -34,22 +40,29 @@ class SpotifySlackBot():
         self.sc.rtm_send_message(event['channel'], "Alright, let's have some silence for now.")
 
     def command_playback_skip(self, event):
-        self.run_spotify_script('playback-skip')
-        self.sc.rtm_send_message(self.broadcast_channel, "*Skipping this song*, as requested by %s." % (self.get_username(event['user'])))
-        self.sc.rtm_send_message(event['channel'], "Sure, let's listen to something else...")
+        if event['user'] not in self.skips:
+            self.skips.add(event['user'])
+            if len(self.skips) >= self.skips_needed:
+                self.run_spotify_script('playback-skip')
+                self.sc.rtm_send_message(self.broadcast_channel, "*Skipping this song*")
+                self.skips = set()
+            else:
+                self.sc.rtm_send_message(self.broadcast_channel, '%s out of %s votes needed to skip' % (len(self.skips), self.skips_needed))
+
+        # self.sc.rtm_send_message(event['channel'], "Sure, let's listen to something else...")
 
     def command_help(self, event):
         self.sc.rtm_send_message(event['channel'],
-                                 "Hey, how are you?  I'm here to help you using our office playlist.\n"
-                                 "I can give you some information about what is playing right now. Just send the command:\n"
+                                 # "Hey, how are you?  I'm here to help you using our office playlist.\n"
+                                 # "I can give you some information about what is playing right now. Just send the command:\n"
                                  "- `song`: I'll tell you which song is playing and who is the artist.\n"
                                  "\n"
-                                 "I can also control the playlist, with the following commands:\n"
-                                 "- `play`: I'll resume playback of the playlist, if it is paused.\n"
-                                 "- `pause`: I'll pause the playback of the playlist, if it is playing.\n"
+                                 # "I can also control the playlist, with the following commands:\n"
+                                 # "- `play`: I'll resume playback of the playlist, if it is paused.\n"
+                                 # "- `pause`: I'll pause the playback of the playlist, if it is playing.\n"
                                  "- `skip`: I'll skip the current song and play another one.\n"
                                  "\n"
-                                 "*Please note:* When you give commands to control the playlist, *I'll advertise on #spotify-playlist that you asked me to do it*, just so everyone knows what is going on. Please use these only if you really need to :)"
+                                 # "*Please note:* When you give commands to control the playlist, *I'll advertise on #spotify-playlist that you asked me to do it*, just so everyone knows what is going on. Please use these only if you really need to :)"
         )
 
     def command_unknown(self, event):
@@ -63,27 +76,42 @@ class SpotifySlackBot():
             if user['id'] == id:
                 return '@%s' % user['name']
         return 'someone'
+
+    def get_curr_song_info(self):
+        data = self.run_spotify_script('current-song')
+        data = data.strip().split('\n')
+        return {"id": data[0], "name": data[1], "artist": data[2]}
+
     
     def run(self):
         commands = [
             ('song', self.command_current_song),
-            ('play', self.command_playback_play),
-            ('pause', self.command_playback_pause),
+            # ('play', self.command_playback_play),
+            # ('pause', self.command_playback_pause),
             ('skip|next', self.command_playback_skip),
             ('hey|help', self.command_help),
             ('.+', self.command_unknown)
         ]
-        
+        # pprint(json.loads(self.sc.api_call('channels.list')))
+        # self.sc.api_call('chat.postMessage', channel=self.broadcast_channel, text='Hello World!')
+        curr_song = ''
         if self.sc.rtm_connect():
             while True:
+                print self.skips
+                prev_song = curr_song
+                curr_song = self.get_curr_song_info()['id']
+                if prev_song != curr_song:
+                    self.skips = set()
                 events = self.sc.rtm_read()
                 for event in events:
-                    print event
-                    if event.get('type') == 'message' and event.get('channel')[0] == 'D':
+                    if event.get('type') == 'message': # and event.get('channel')[0] == 'C':
                         for (expression, function) in commands:
-                            if re.match(expression, event['text']):
-                                function(event)
-                                break
+                            try:
+                                if re.match(expression, event['text']):
+                                    function(event)
+                                    break
+                            except:
+                                pass
                 time.sleep(1)
 
 if __name__ == '__main__':
